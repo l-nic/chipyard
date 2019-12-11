efc2ca3a2da447d2dedd80ff33275ce8db29291e
Stephen Ibanez <sibanez@g.hmc.edu>
2019-12-11 00:23:30 +0000

    Updated L-NIC loopback program to instead write the first word as msg length.

--- tests/simple-lnic-loopback-gpr.c
+++ tests/simple-lnic-loopback-gpr.c
@@ -6,19 +6,8 @@
 
 // LNIC CSRs
 #define CSR_LMSGSRDY 0x052
-#define CSR_LWREND 0x058
 
 #define PKT_LEN 8
-
-static inline void lnic_write(uint64_t word) {
-	asm("mv x30, a0");
-}
-
-static inline uint64_t lnic_read() {
-	register uint64_t to_return asm("a0");
-	asm("mv a0, x31");
-	return to_return;
-}
 
 int main(void)
 {
@@ -28,24 +17,19 @@ int main(void)
 
 	start = rdcycle();
 	// send pkt
-	asm ("li x31, 0");
+	asm ("li x31, 64"); // msg len
 	asm ("li x31, 1");
 	asm ("li x31, 2");
 	asm ("li x31, 3");
 	asm ("li x31, 4");
 	asm ("li x31, 5");
 	asm ("li x31, 6");
-	write_csr(0x058, 1); // write lwrend
 	asm ("li x31, 7");
+	asm ("li x31, 8");
 	// receive pkt
-	//while (read_csr(CSR_LMSGSRDY) == 0);
-	while (read_csr(0x052) == 0);
-	for (i = 0; i < PKT_LEN; i++) {
+	while (read_csr(0x052) == 0); // poll CSR_LMSGSRDY
+	for (i = 0; i < PKT_LEN+1; i++) { // 8 words + msg_len word
 		asm ("mv t0, x30");
-//		if (data != i) {
-//			printf("Data mismatch: %lu != %d\n", data, i);
-//			exit(EXIT_FAILURE);
-//		}
 	}
 	end = rdcycle();
 	
