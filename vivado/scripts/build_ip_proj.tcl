0623fb86e9cd3827ac7d90406e715c6584442dc8
Stephen Ibanez <sibanez@g.hmc.edu>
2020-03-23 18:51:58 +0000

    Adding top-level vivado/ directory with scripts to generate SDNet IP.
    Also updated vcs Makefile to compile the design with SDNet.
    
    It compiles in a _mostly_ automated way but there are still some
    manual steps required.
    
    Compilation steps:
    1. Source chipyard/env.sh
    2. cd vivado/scripts
    3. Build Vivado IP project: vivado -mode batch -source build_ip_proj.tcl
    4. Build SDNet IP: vivado -mode batch -source build_sdnet.tcl
    5. Run the generated script to build SDNet IP with vcs:
       cd chipyard/vivado/ip/ip_user_files/sim_scripts/sdnet_0/vcs && ./sdnet_0.sh
    6. Compile full design with vcs:
       cd chipyard/sims/vcs && make CONFIG=SimNetworkLNICGPRConfig TOP=TopWithLNIC -j16
       For some reason, the final step in compiling the executable fails when using the
       Makefile, but if the command is run manually then it seems to work fine. So,
       run the command manually:
       rm -rf csrc && vcs -full64 riscv_lib.TestDriver -o /home/vagrant/chipyard/sims/vcs/simv-example-SimNetworkLNICGPRConfig -debug_pp
    7. Run the simulation while linking to the compiled DPI library:
       ./simv-example-SimNetworkLNICGPRConfig +permissive -sv_lib riscv_dpi +permissive-off ../../tests/hello.riscv

---
+++ vivado/scripts/build_ip_proj.tcl
@@ -0,0 +1,12 @@
+
+# Build Managed IP project and Compile Xilinx simulation libraries with VCS
+
+set vcs_path /cad/synopsys/vcs/P-2019.06-SP2-2/bin
+set proj_dir $::env(CHIPYARD_ROOT)/vivado/ip/managed_ip_project
+
+create_project managed_ip_project ${proj_dir} -part xcu250-figd2104-2L-e -ip
+set_property target_simulator VCS [current_project]
+
+# Compile simulation libraries
+compile_simlib -simulator vcs -simulator_exec_path ${vcs_path} -family all -language all -library all -dir ${proj_dir}/managed_ip_project.cache/compile_simlib/vcs -force
+
---
+++ vivado/scripts/build_sdnet.tcl
@@ -0,0 +1,35 @@
+
+# Build SDNet IP
+
+set p4_file $::env(CHIPYARD_ROOT)/vivado/p4src/lnic_v1.p4
+set build_dir $::env(CHIPYARD_ROOT)/vivado/ip
+set proj_dir ${build_dir}/managed_ip_project
+set sdnet sdnet_0
+set xci_file ${build_dir}/${sdnet}/${sdnet}.xci
+
+open_project ${proj_dir}/managed_ip_project.xpr
+
+# Delete the sdnet IP if it already exists
+if {![string equal [get_ips $sdnet] ""]} {
+    puts "INFO: Deleting current ${sdnet} IP"
+    remove_files ${xci_file}
+    file delete -force ${build_dir}/ip/${sdnet}
+}
+
+create_ip -name sdnet -vendor xilinx.com -library ip -version 2.1 -module_name $sdnet -dir $build_dir
+set_property -dict [subst {
+    CONFIG.TDATA_NUM_BYTES {8}
+    CONFIG.P4_FILE {${p4_file}}
+    CONFIG.DEBUG_IO_CAPTURE_ENABLE {true}
+    CONFIG.CAM_DEBUG_HW_LOOKUP {true}
+    CONFIG.CAM_MEM_CLK_FREQ_MHZ {250.0}
+    CONFIG.AXIS_CLK_FREQ_MHZ {250.0}
+    CONFIG.PKT_RATE {250.0}
+    CONFIG.DIRECT_TABLE_PARAMS {}
+    CONFIG.CAM_TABLE_PARAMS {}
+}] [get_ips $sdnet]
+generate_target all [get_ips $sdnet]
+
+export_ip_user_files -of_objects [get_files ${xci_file}] -no_script -sync -force -quiet
+export_simulation -absolute_path -of_objects [get_files ${xci_file}] -directory ${build_dir}/ip_user_files/sim_scripts -ip_user_files_dir ${build_dir}/ip_user_files -ipstatic_source_dir ${build_dir}/ip_user_files/ipstatic -lib_map_path vcs=${build_dir}/managed_ip_project/managed_ip_project.cache/compile_simlib/vcs -use_ip_compiled_libs -force -quiet
+
