5e3f8af13e390a92eac62cbd5f323ef3b6ac4981
Stephen Ibanez <sibanez@g.hmc.edu>
2020-04-25 00:12:59 +0000

    Bumping rocket-chip also update LNIC headers

--- software/net-app-tester/LNIC_headers.py
+++ software/net-app-tester/LNIC_headers.py
@@ -7,22 +7,14 @@ LNIC_PROTO = 0x99
 class LNIC(Packet):
     name = "LNIC"
     fields_desc = [
-        ShortField("src", 0),
-        ShortField("dst", 0),
-        ShortField("msg_id", 0),
-        # msg_len will default to size of payload
-        ShortField("msg_len", None),
-        ShortField("offset", 0),
-        IntField("padding", 0)
+        ShortField("src_context", 0),
+        ShortField("dst_context", 0),
+        ShortField("msg_len", 0),
+        ByteField("pkt_offset", 0),
+        ShortField("tx_msg_id", 0),
+        ShortField("buf_ptr", 0),
+        ByteField("buf_size_class", 0)
     ]
-    def post_build(self, p, pay):
-        if self.msg_len is None:
-            l = len(pay)
-            p = p[:6]+struct.pack("!H", l)+p[8:]
-        return p+pay
-    def mysummary(self):
-        return self.sprintf("src=%src% dst=%dst% msg_id=%msg_id% msg_len=%msg_len% offset=%offset%")
-    
 
 bind_layers(IP, LNIC, proto=LNIC_PROTO)
 
