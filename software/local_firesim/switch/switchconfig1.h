5b9336384ec954563d4e44d87fe30c021a971cf0
Stephen Ibanez <sibanez@g.hmc.edu>
2020-08-09 19:42:55 +0000

    Updated switchconfig1.h to include updated load generator params.

--- software/local_firesim/switch/switchconfig1.h
+++ software/local_firesim/switch/switchconfig1.h
@@ -17,19 +17,36 @@
 
     #ifdef LOADGENSTATS
     #define USE_LOAD_GEN
-    char* test_type = "ONE_CONTEXT_FOUR_CORES";
-    char* dist_type = "FIXED";
-    uint64_t request_rate_lambda_inverse = 5000;
+    // Load balancing tests
+//    char* test_type = "ONE_CONTEXT_FOUR_CORES";
+//    char* test_type = "FOUR_CONTEXTS_FOUR_CORES";
+
+    // Thread scheduling tests
+    char* test_type = "DIF_PRIORITY_LNIC_DRIVEN";
+//    char* test_type = "DIF_PRIORITY_TIMER_DRIVEN";
+
+    // Bounded processing time tests
+//    char* test_type = "HIGH_PRIORITY_C1_STALL";
+//    char* test_type = "LOW_PRIORITY_C1_STALL";
+
+    // Distribution parameters
+//    char* dist_type = "BIMODAL";
+    char* service_dist_type = "FIXED";
+    char* request_dist_type = "EXP";
+    uint64_t num_requests = 10;
+    uint64_t request_rate_lambda_inverse_start = 3200;
+    uint64_t request_rate_lambda_inverse_stop = 3200;
+    uint64_t request_rate_lambda_inverse_dec = 0;
     uint64_t min_service_time = 300;
     uint64_t max_service_time = 10000;
     double exp_dist_scale_factor = 10000;
     double exp_dist_decay_const = 3.5;
-    double bimodal_dist_high_mean = 5000;
-    double bimodal_dist_high_stdev = 300;
-    double bimodal_dist_low_mean = 500;
-    double bimodal_dist_low_stdev = 30;
-    double bimodal_dist_fraction_high = 0.5;
-    uint64_t fixed_dist_cycles = 800;
+    double bimodal_dist_high_mean = 9000;
+    double bimodal_dist_high_stdev = 100;
+    double bimodal_dist_low_mean = 1600;
+    double bimodal_dist_low_stdev = 100;
+    double bimodal_dist_fraction_high = 0.2;
+    uint64_t fixed_dist_cycles = 1600;
     uint16_t rtt_pkts = 2;
     #endif
 
