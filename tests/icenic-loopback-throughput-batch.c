c40d7d72368de023b15e4908a9a2d977df78989a
Stephen Ibanez <sibanez@g.hmc.edu>
2020-11-26 22:22:42 +0000

    Updated icenic-loopback-throughput-batch to try and overlap computation (i.e. memcpys) and DMA rx/tx operations.

--- tests/icenic-loopback-throughput-batch.c
+++ tests/icenic-loopback-throughput-batch.c
@@ -5,20 +5,26 @@
 #include "mmio.h"
 #include "icenic.h"
 
-#define NUM_BUFS 10
+#define NUM_BUFS 50
 #define TSTAMP_BYTES 8
 
 uint64_t buffers[NUM_BUFS][ETH_MAX_WORDS];
 
-static void nic_send_batch(uint64_t bufs[][ETH_MAX_WORDS], uint64_t *lens, int tx_count)
+/* Post a send request
+ */
+static void nic_post_send(void *buf, uint64_t len)
+{
+  uintptr_t addr = ((uintptr_t) buf) & ((1L << 48) - 1);
+  unsigned long packet = (len << 48) | addr;
+  while (nic_send_req_avail() == 0);
+  reg_write64(SIMPLENIC_SEND_REQ, packet);
+}
+
+/* Wait for all send operations to complete
+ */
+static void nic_wait_send_batch(int tx_count)
 {
   int i;
-  for (i=0; i < tx_count; i++) {
-    uintptr_t addr = ((uintptr_t) bufs[i]) & ((1L << 48) - 1);
-    unsigned long packet = (lens[i] << 48) | addr;
-    while (nic_send_req_avail() == 0);
-    reg_write64(SIMPLENIC_SEND_REQ, packet);
-  }
 
   for (i=0; i < tx_count; i++) {
     // Poll for completion
@@ -27,7 +33,9 @@ static void nic_send_batch(uint64_t bufs[][ETH_MAX_WORDS], uint64_t *lens, int t
   }
 }
 
-static void nic_recv_batch(uint64_t bufs[][ETH_MAX_WORDS], int rx_count, uint64_t *lens)
+/* Post a batch of RX requests
+ */
+static void nic_post_recv_batch(uint64_t bufs[][ETH_MAX_WORDS], int rx_count)
 {
   int i;
   for (i=0; i < rx_count; i++) {
@@ -35,49 +43,60 @@ static void nic_recv_batch(uint64_t bufs[][ETH_MAX_WORDS], int rx_count, uint64_
     while (nic_recv_req_avail() == 0);
     reg_write64(SIMPLENIC_RECV_REQ, addr);
   }
+}
 
-  for (i=0; i < rx_count; i++) {
-    // Poll for completion
-    while (nic_recv_comp_avail() == 0);
-    lens[i] = reg_read16(SIMPLENIC_RECV_COMP);
-    asm volatile ("fence");
-  }
+/* Wait for a receive to complete
+ */
+static int nic_wait_recv()
+{
+  int len;
+
+  // Poll for completion
+  while (nic_recv_comp_avail() == 0);
+  len = reg_read16(SIMPLENIC_RECV_COMP);
+  asm volatile ("fence");
+
+  return len;
 }
 
 int main(void)
 {
   printf("Ready!\n");
-  uint64_t lens[NUM_BUFS]; // store the lengths of each packet
-  uint8_t *buf_ptr;
   uint8_t start_time[TSTAMP_BYTES];
   int i;
+  int len;
 
   // receive 1st batch of pkt
-  nic_recv_batch(buffers, NUM_BUFS, lens);
+  nic_post_recv_batch(buffers, NUM_BUFS);
+  // process 1st pkt
+  len = nic_wait_recv();
   // extract timestamp from 1st pkt
   swap_eth(buffers[0]);
-  buf_ptr = buffers[0];
-  memcpy(start_time, buf_ptr + lens[0] - TSTAMP_BYTES, TSTAMP_BYTES);
+  memcpy(start_time, (void *)buffers[0] + len - TSTAMP_BYTES, TSTAMP_BYTES);
+  // send 1st pkt out
+  nic_post_send(buffers[0], len);
   // copy timestamp into subsequent pkts
   for (i=1; i < NUM_BUFS; i++) {
+    len = nic_wait_recv();
     swap_eth(buffers[i]);
-    buf_ptr = buffers[i];
-    memcpy(buf_ptr + lens[i] - TSTAMP_BYTES, start_time, TSTAMP_BYTES);
+    memcpy((void *)buffers[i] + len - TSTAMP_BYTES, start_time, TSTAMP_BYTES);
+    nic_post_send(buffers[i], len);
   }
-  // send batch of pkts back out 
-  nic_send_batch(buffers, lens, NUM_BUFS);
+  // wait for all send operations to complete
+  nic_wait_send_batch(NUM_BUFS);
 
   // forward all subsequent pkts and copy over start timestamp
   while (1) {
-    nic_recv_batch(buffers, NUM_BUFS, lens);
+    nic_post_recv_batch(buffers, NUM_BUFS);
     for (i=0; i < NUM_BUFS; i++) {
+      len = nic_wait_recv();
       swap_eth(buffers[i]);
       // copy over start timestamp
-      buf_ptr = buffers[i];
-      memcpy(buf_ptr + lens[i] - TSTAMP_BYTES, start_time, TSTAMP_BYTES);
+      memcpy((void *)buffers[i] + len - TSTAMP_BYTES, start_time, TSTAMP_BYTES);
+      nic_post_send(buffers[i], len);
     }
-    // send pkt back out
-    nic_send_batch(buffers, lens, NUM_BUFS);
+    // wait for all send operations to complete
+    nic_wait_send_batch(NUM_BUFS);
   }
 
   return 0;
