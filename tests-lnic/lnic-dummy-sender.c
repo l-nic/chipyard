4a0f4082ea8f1adcb237c72a59e9c68afeb516d0
Serhat Arslan <sarslan@stanford.edu>
2021-04-17 01:50:44 +0000

    spin proto-eval workload to prevent early return

--- tests-lnic/lnic-dummy-sender.c
+++ tests-lnic/lnic-dummy-sender.c
@@ -4,7 +4,7 @@
 
 #include "lnic.h"
 
-#define MSG_LEN_PKTS 9
+#define MSG_LEN_PKTS 33
 
 #define REPEAT_4(X) X X X X
 #define REPEAT_8(X) REPEAT_4(X) REPEAT_4(X)
@@ -13,7 +13,7 @@
 #define REPEAT_64(X) REPEAT_32(X) REPEAT_32(X)
 #define REPEAT_128(X) REPEAT_64(X) REPEAT_64(X)
 
-int main(void)
+int main(int argc, char** argv)
 {
   uint64_t app_hdr;
   int i; 
@@ -24,19 +24,32 @@ int main(void)
   uint64_t priority = 0;
   lnic_add_context(context_id, priority);
 
-  printf("Ready!\n");
-  // send msg
-  app_hdr = (dst_ip << 32) | (MSG_LEN_PKTS*1024);
-  lnic_write_r(app_hdr);
-  for (i = 0; i < MSG_LEN_PKTS; i++) {
-    REPEAT_128(lnic_write_i(0);)
-  }
+  char* nic_ip_str = argv[2];
+  uint32_t nic_ip_addr_lendian = 0;
+  int retval = inet_pton4(nic_ip_str, nic_ip_str + strlen(nic_ip_str), &nic_ip_addr_lendian);
+  uint32_t nic_ip_addr = swap32(nic_ip_addr_lendian);
 
-  printf("Done!\n");
-  // wait for a bit before exiting
-  for (i = 0; i < 500; i++) {
-    asm volatile("nop");
+  printf("%x Ready!\n", nic_ip_addr);
+
+  if (nic_ip_addr == 0x0a000003) {
+    printf("Sending msg!\n");
+    // send msg
+    app_hdr = (dst_ip << 32) | (MSG_LEN_PKTS*1024);
+    lnic_write_r(app_hdr);
+    for (i = 0; i < MSG_LEN_PKTS; i++) {
+      REPEAT_128(lnic_write_i(0);)
+    }
+
+    printf("Done!\n");
   }
+  // // wait for a bit before exiting
+  // for (i = 0; i < 500; i++) {
+  //   asm volatile("nop");
+  // }
+
+  // spin for the rest of the simulation to allow others to run
+  while(1);
+
   return 0;
 }
 
